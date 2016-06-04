a7b92f8d2bc98a85147bf75c2e7c0c5e82ea4b62
bugfi5h <rene.kling1992@gmail.com>
2016-06-04 10:07:30 +0200

    Projekt für Spiel hinzugefügt, sowie GameConstants Klasse

---
+++ AdvancedWars/AdvancedWars/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AdvancedWars
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
+
+
---
+++ AdvancedWars/AdvancedWars/GameConstants.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdvancedWars
+{
+    public class GameConstants
+    {
+        public const int PLAYER_STARTINGGOLD = 100;
+
+        public const string TYPE_BIGSHIP = "Bigship";
+        public const string TYPE_TRANSPORTER = "Transporter";
+        public const string TYPE_DEFENSESHIP = "Defense";
+        public const string TYPE_STANDART = "Standart";
+
+        public const int SHIP_HEALTH = 100;
+        public const int SHIP_DEFENSE = 100;
+        public const int SHIP_MOVEMENT = 100;
+        public const int SHIP_COST = 100;
+        public const int SHIP_DAMAGE = 100;
+
+        public const int BIGSHIP_HEALTHMOD = -30;
+        public const int BIGSHIP_DEFENSEHMOD = -30;
+        public const int BIGSHIP_MOVEMENTMOD = -30;
+        public const int BIGSHIP_COSTMOD = -30;
+        public const int BIGSHIP_DAMAGEMOD = -30;
+
+        public const int TRANSPORTER_HEALTHMOD = -30;
+        public const int TRANSPORTER_DEFENSEHMOD = -30;
+        public const int TRANSPORTER_MOVEMENTMOD = -30;
+        public const int TRANSPORTER_COSTMOD = -30;
+        public const int TRANSPORTER_DAMAGEMOD = -30;
+
+        public const int DEFENSE_HEALTHMOD = -30;
+        public const int DEFENSE_DEFENSEHMOD = -30;
+        public const int DEFENSE_MOVEMENTMOD = -30;
+        public const int DEFENSE_COSTMOD = -30;
+        public const int DEFENSE_DAMAGEMOD = -30;
+
+     
+    }
+}
