1ba47bc5089ed0f622e6a195c557b8a142ea93f3
nadine114 <nadine11becker@web.de>
2016-06-09 07:18:31 +0000

    Add files via upload

---
+++ AdvancedWars/AdvancedWars/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdvancedWars
+{
+    public class Player
+    {
+        private int mGold;
+        private string mName;
+        private int mShipMoved;
+
+        public Player(string name)
+        {
+            mGold = GameConstants.PLAYER_STARTINGGOLD;
+            mName = name;
+            mShipMoved=0;
+        }
+
+        public int Gold
+        {
+            get { return mGold; }
+            set { mGold = value; }
+        }
+
+        public string Name
+        {
+            get { return mName; }
+        }
+
+        public int ShipMoved
+        {
+            get { return mShipMoved; }
+            set { mShipMoved = value; }
+        }
+    }
+}
